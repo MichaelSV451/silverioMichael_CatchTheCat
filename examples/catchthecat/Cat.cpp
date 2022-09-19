100094c42665bd153c42ce8057506341cc20d8f2
Michael Silverio <michael.silverio@mymail.champlain.edu>
2022-09-19 14:10:14 -0400

    assignment 2 cloned

--- examples/catchthecat/Cat.cpp
+++ examples/catchthecat/Cat.cpp
@@ -5,6 +5,10 @@
 Point2D Cat::Move(World* world) {
   auto rand = Random::Range(0,5);
   auto pos = world->getCat();
+
+  //follow shortest path to exit
+
+
   switch(rand){
     case 0:
       return World::NE(pos);
