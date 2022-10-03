78f10ff7556fd0a2e3497737bed4c2595614db73
Michael Silverio <michael.silverio@mymail.champlain.edu>
2022-10-03 14:16:08 -0400

    working

--- examples/catchthecat/Cat.cpp
+++ examples/catchthecat/Cat.cpp
@@ -7,57 +7,102 @@
 using namespace std;
 
 Point2D Cat::Move(World* world) {
-  auto rand = Random::Range(0,5);
+  auto rand = Random::Range(0, 5);
   auto pos = world->getCat();
 
-  const Vector2 NORTHWEST;
-  const Vector2 NORTHEAST;
-  const Vector2 EAST = {1, 0};
-  const Vector2 SOUTHEAST;
-  const Vector2 SOUTHWEST;
-  const Vector2 WEST = {-1, 0};
-  
+  const int DIRECTIONS = 5;
 
-  //clear the map
   visited.clear();
   from.clear();
 
-  //start queue with position of the cat
   vector<QueueEntry> queue;
-  queue.push_back({world->getCat(), 0});
+  queue.push_back({pos, 0});
 
-  //main loop
+  auto queueHead = queue[0];
+
   while (!queue.empty()) {
-      //sort from beginning to end
+
+    //sort queue
     sort(queue.begin(), queue.end());
+ 
+    queueHead = queue[0];
 
     //remove head
     queue.erase(queue.begin());
 
-  // //look ne
-  // if(world->isValidPosition(World::NE(pos))) 
-  //     queue.push_back({World::NE(pos), 1});
-  // // look nw
-  // if (world->isValidPosition(World::NW(pos))) 
-  //     queue.push_back({World::NW(pos), 1});
-  // //look e
-  // if (world->isValidPosition(World::E(pos)))
-  //   queue.push_back({World::E(pos), 1});
-  // //look se
-  // if (world->isValidPosition(World::SE(pos)))
-  //   queue.push_back({World::SE(pos), 1});
-  // //look sw
-  // if (world->isValidPosition(World::SW(pos)))
-  //   queue.push_back({World::SW(pos), 1});
-  // //look w
-  // if (world->isValidPosition(World::W(pos)))
-  //   queue.push_back({World::W(pos), 1});
-  // //mark head as visited
+    //marked head visited
+    visited[queueHead.position.x][queueHead.position.y] = true;
 
-    //for each neighbor:
-    //if not visited add to the queue with 1 weight
-    //mark where this element comes from
+    for (int i = 0; i <= DIRECTIONS; i++) {
+      Point2D search = {0, 0};
+
+      //set search point
+      switch (i) {
+        case 0:
+          search = World::NE(queueHead.position);
+          break;
+        case 1:
+          search = World::NW(queueHead.position);
+          break;
+        case 2:
+          search = World::E(queueHead.position);
+          break;
+        case 3:
+          search = World::W(queueHead.position);
+          break;
+        case 4:
+          search = World::SW(queueHead.position);
+          break;
+        case 5:
+          search = World::SE(queueHead.position);
+          break;
+      }
+
+      //if not visited, and is valid position,
+      if (!world->getContent(search) && visited[search.x][search.y] == false && world->isValidPosition(search)) {
+
+          cout << "pushing back " << search.x << " , " << search.y << endl;
+
+          //add it to the queue
+          queue.push_back({search, queueHead.weight + 1});
+
+          //set from
+          from[search.x][search.y] = queueHead.position;
+      }
+    }
+
+    cout << queue.size() << endl;
   }
+
+ ////clear the map
+ //
+ //
+ ////start queue with position of the cat
+ //vector<QueueEntry> queue;
+ //
+ //from[world->getCat().x][world->getCat().y] = world->getCat();
+ //visited[world->getCat().x][world->getCat().y] = true;
+ //queue.push_back({world->getCat(), 0});
+ //
+ ////main loop
+ //while (!queue.empty()) {
+ //  //sort from beginning to end
+ //  sort(queue.begin(), queue.end());
+ //
+ //  //remove head
+ //  queue.erase(queue.begin());
+ //
+ //  
+ //
+ //  cout << queue.size() << endl;
+ //
+ //
+ //  //mark head as visited
+ //
+ //  //for each neighbor:
+ //  //if not visited add to the queue with 1 weight
+ //  //mark where this element comes from
+ //}
 
 
   //use pathfinding to get shortest route towards an exit point
