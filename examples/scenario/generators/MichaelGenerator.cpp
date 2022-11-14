ad0729e046c341a4b14eadde0c60fa6131f0a3af
Michael Silverio <michael.silverio@mymail.champlain.edu>
2022-11-14 12:49:17 -0500

    noise tesT

--- examples/scenario/generators/MichaelGenerator.cpp
+++ examples/scenario/generators/MichaelGenerator.cpp
@@ -2,6 +2,7 @@
 #include "../PerlinNoise.hpp"
 #include "MichaelGenerator.h"
 #include <iostream>
+
 // do not use this one to your assignment. this is my sample generator
 std::vector<Color32> MichaelGenerator::Generate(int sideSize,
                                                        float displacement) {
@@ -12,9 +13,18 @@ std::vector<Color32> MichaelGenerator::Generate(int sideSize,
   //  Noise noise(1337, 1024,0,255);
   for (int l = 0; l < sideSize; l++) {
     for (int c = 0; c < sideSize; c++) {
-      float rgb =
-          abs(noise.octave3D(c / 15.0, l / 450.0, displacement, 2) * 255);
-      colors.emplace_back(rgb*.5, rgb*.85, 255);
+     
+        
+      float rgb = abs(noise.octave3D(c / 50.0, l / 50.0, displacement, 2) * 255);
+        
+      if (rgb < 20.0) colors.emplace_back(0, 0, 255);
+      else if (rgb >= 20 && rgb < 80)
+        colors.emplace_back(0, 255, 0);
+      else if (rgb >= 80 && rgb < 175)
+        colors.emplace_back(50, 50, 50);
+      else
+        colors.emplace_back(255, 255, 255);
+      
       //double color = noise.noise2D(c/15.0,l/450.0);
       //colors.emplace_back(color,color,color);
     }
